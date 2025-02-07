9b4556bdac7cd609c4de06f4573a4c6473505410
psabhishek26 <sploitpay.aosp.test@gmail.com>
2025-02-07 11:43:52 +0530

    Import custom font `Poppins` for expo
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- App.js
+++ App.js
@@ -1,8 +1,35 @@
+import { useEffect } from "react";
 import Navigators from "./src/navigators";
 import { store } from "./src/Store";
 import { Provider } from "react-redux";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    "Poppins-Black": require("./src/assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Regular": require("./src/assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Bold": require("./src/assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Medium": require("./src/assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Light": require("./src/assets/fonts/Poppins-Light.ttf"),
+    "Poppins-SemiBold": require("./src/assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("./src/assets/fonts/Poppins-Thin.ttf"),
+    "Poppins-ExtraBold": require("./src/assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("./src/assets/fonts/Poppins-ExtraLight.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <Provider store={store}>
       <Navigators />
