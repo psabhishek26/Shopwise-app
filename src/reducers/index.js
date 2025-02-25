dad3d7a82d6ae8d20ed10c0c22f846867710ab07
psabhishek26 <sploitpay.aosp.test@gmail.com>
2025-02-25 13:55:16 +0530

    Cleanup useless redux state
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,8 @@ import { combineReducers } from "@reduxjs/toolkit";
 
 import GeneralReducer from "./GeneralReducer";
 import CartReducer from "./CartReducer";
-import BookmarkReducer from "./BookmarkReducer";
 
 export default combineReducers({
   generalState: GeneralReducer,
   cartState: CartReducer,
-  bookmarkState: BookmarkReducer,
 });
