a7b04e1c5ed6c8e3d59f7eaff6b91d255a589f9d
psabhishek26 <sploitpay.aosp.test@gmail.com>
2025-02-25 14:12:46 +0530

    Redo Account Screen based on shopwise requirements
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -3,22 +3,20 @@ import {
   View,
   Text,
   StyleSheet,
-  StatusBar,
   Image,
   TouchableOpacity,
 } from "react-native";
-import { Separator, ToggleButton } from "../components";
 import { Colors, Fonts, Images } from "../contants";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import Feather from "@expo/vector-icons/Feather";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { Display } from "../utils";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { StorageService } from "../services";
 import { GeneralAction } from "../actions";
 
 const AccountScreen = ({ navigation }) => {
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.generalState.userData.data);
 
   const logout = () => {
     StorageService.setToken("").then(() => {
@@ -29,12 +27,6 @@ const AccountScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor={Colors.DEFAULT_GREEN}
-        translucent
-      />
-      <Separator height={StatusBar.currentHeight} />
       <View style={styles.backgroundCurvedContainer} />
       <View style={styles.headerContainer}>
         <Ionicons
@@ -44,20 +36,14 @@ const AccountScreen = ({ navigation }) => {
           onPress={() => navigation.goBack()}
         />
         <Text style={styles.headerText}>Account</Text>
-        <View>
-          <Feather name="bell" size={20} color={Colors.DEFAULT_WHITE} />
-          <View style={styles.alertBadge}>
-            <Text style={styles.alertBadgeText}>12</Text>
-          </View>
-        </View>
       </View>
       <View style={styles.profileHeaderContainer}>
         <View style={styles.profileImageContainer}>
           <Image style={styles.profileImage} source={Images.AVATAR} />
         </View>
         <View style={styles.profileTextContainer}>
-          <Text style={styles.nameText}>Ameen Farook</Text>
-          <Text style={styles.emailText}>ameeen.faroook@gmail.com</Text>
+          <Text style={styles.nameText}>{user.username}</Text>
+          <Text style={styles.emailText}>{user.email}</Text>
         </View>
       </View>
       <View style={styles.menuContainer}>
@@ -69,7 +55,7 @@ const AccountScreen = ({ navigation }) => {
               color={Colors.DEFAULT_GREEN}
             />
           </View>
-          <Text style={styles.menuText}>My All {"\n"}Orders</Text>
+          <Text style={styles.menuText}>Orders</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.menuItem} activeOpacity={0.8}>
           <View
@@ -81,7 +67,7 @@ const AccountScreen = ({ navigation }) => {
               color={Colors.SECONDARY_RED}
             />
           </View>
-          <Text style={styles.menuText}>Offers {"&\n"} Promos</Text>
+          <Text style={styles.menuText}>Offers</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.menuItem} activeOpacity={0.8}>
           <View
@@ -93,70 +79,10 @@ const AccountScreen = ({ navigation }) => {
               color={Colors.DEFAULT_YELLOW}
             />
           </View>
-          <Text style={styles.menuText}>Delivery Addresses</Text>
+          <Text style={styles.menuText}>Addresses</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.mainContainer}>
-        <Text style={styles.sectionHeaderText}>My Account</Text>
-        <TouchableOpacity style={styles.sectionContainer} activeOpacity={0.8}>
-          <View style={styles.sectionTextContainer}>
-            <Ionicons
-              name="person-outline"
-              size={18}
-              color={Colors.DEFAULT_GREEN}
-            />
-            <Text style={styles.sectionText}>Manage Profile</Text>
-          </View>
-          <Feather
-            name="chevron-right"
-            color={Colors.INACTIVE_GREY}
-            size={20}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.sectionContainer} activeOpacity={0.8}>
-          <View style={styles.sectionTextContainer}>
-            <Ionicons
-              name="card-outline"
-              size={18}
-              color={Colors.DEFAULT_GREEN}
-            />
-            <Text style={styles.sectionText}>Payment</Text>
-          </View>
-          <Feather
-            name="chevron-right"
-            color={Colors.INACTIVE_GREY}
-            size={20}
-          />
-        </TouchableOpacity>
-
-        <Text style={styles.sectionHeaderText}>Notification</Text>
-        <View style={styles.sectionContainer} activeOpacity={0.8}>
-          <View style={styles.sectionTextContainer}>
-            <Feather name="bell" size={18} color={Colors.DEFAULT_GREEN} />
-            <Text style={styles.sectionText}>Notification</Text>
-          </View>
-          <ToggleButton size={0.5} />
-        </View>
-        <View style={styles.sectionContainer} activeOpacity={0.8}>
-          <View style={styles.sectionTextContainer}>
-            <Feather name="bell" size={18} color={Colors.DEFAULT_GREEN} />
-            <Text style={styles.sectionText}>Promos & Offers Notification</Text>
-          </View>
-          <ToggleButton size={0.5} />
-        </View>
-
-        <Text style={styles.sectionHeaderText}>More</Text>
-        <View style={styles.sectionContainer} activeOpacity={0.8}>
-          <View style={styles.sectionTextContainer}>
-            <Ionicons
-              name="ios-color-palette-outline"
-              size={18}
-              color={Colors.DEFAULT_GREEN}
-            />
-            <Text style={styles.sectionText}>Dark Mode</Text>
-          </View>
-          <ToggleButton size={0.5} />
-        </View>
         <View style={styles.sectionContainer}>
           <TouchableOpacity
             style={styles.sectionTextContainer}
@@ -189,11 +115,9 @@ const styles = StyleSheet.create({
     width: 2000,
     borderRadius: 2000,
     alignSelf: "center",
-    zIndex: -1,
   },
   headerContainer: {
     flexDirection: "row",
-    justifyContent: "space-between",
     alignItems: "center",
     paddingVertical: 10,
     paddingHorizontal: 20,
@@ -203,23 +127,7 @@ const styles = StyleSheet.create({
     fontFamily: Fonts.POPPINS_MEDIUM,
     lineHeight: 20 * 1.4,
     color: Colors.DEFAULT_WHITE,
-  },
-  alertBadge: {
-    backgroundColor: Colors.DEFAULT_YELLOW,
-    position: "absolute",
-    height: 16,
-    width: 16,
-    borderRadius: 32,
-    justifyContent: "center",
-    alignItems: "center",
-    right: -2,
-    top: -10,
-  },
-  alertBadgeText: {
-    fontSize: 10,
-    fontFamily: Fonts.POPPINS_BOLD,
-    lineHeight: 10 * 1.4,
-    color: Colors.DEFAULT_WHITE,
+    marginLeft: 10,
   },
   profileHeaderContainer: {
     marginHorizontal: 20,
@@ -244,13 +152,13 @@ const styles = StyleSheet.create({
     marginLeft: 10,
   },
   nameText: {
-    fontSize: 14,
-    fontFamily: Fonts.POPPINS_REGULAR,
+    fontSize: 18,
+    fontFamily: Fonts.POPPINS_MEDIUM,
     lineHeight: 14 * 1.4,
     color: Colors.DEFAULT_WHITE,
   },
   emailText: {
-    fontSize: 10,
+    fontSize: 11,
     fontFamily: Fonts.POPPINS_REGULAR,
     lineHeight: 10 * 1.4,
     color: Colors.DEFAULT_WHITE,
@@ -292,20 +200,12 @@ const styles = StyleSheet.create({
     elevation: 3,
     paddingHorizontal: 20,
     borderRadius: 10,
-    paddingBottom: 20,
-  },
-  sectionHeaderText: {
-    fontSize: 14,
-    fontFamily: Fonts.POPPINS_SEMI_BOLD,
-    lineHeight: 14 * 1.4,
-    color: Colors.DEFAULT_BLACK,
-    marginTop: 25,
+    paddingVertical: 20,
   },
   sectionContainer: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    marginTop: 15,
   },
   sectionTextContainer: {
     flexDirection: "row",
