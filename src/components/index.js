3b691584e8104ebe7d9313e001c695403d8a0a66
psabhishek26 <sploitpay.aosp.test@gmail.com>
2025-03-04 20:18:17 +0530

    Remove unwanted restaurant related components
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,11 @@
 import WelcomeCard from "./WelcomeCard";
 import Separator from "./Separator";
 import ToggleButton from "./ToggleButton";
-import RestaurantCard from "./RestaurantCard";
-import RestaurantMediumCard from "./RestaurantMediumCard";
 import FoodCard from "./FoodCard";
 
 export {
   WelcomeCard,
   Separator,
   ToggleButton,
-  RestaurantCard,
-  RestaurantMediumCard,
   FoodCard,
 };
